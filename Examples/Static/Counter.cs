16b4bd7c9f35f52338e5e67e095e663841808325
Daniel <dhcgn@users.noreply.github.com>
2020-01-27 11:34:03 +0100

    Add project

---
+++ Examples/Static/Counter.cs
@@ -0,0 +1,12 @@
+﻿namespace Static
+{
+    class Counter
+    {
+        public int counter = 0;
+
+        public void Add(int i)
+        {
+            counter += i;
+        }
+    }
+}
---
+++ Examples/Static/Settings.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Xml.Linq;
+
+namespace Static
+{
+    public class Settings
+    {
+        public const string SettingsPath = @"C:\temp\settings.json";
+
+        public String AppName { get; set; }
+        public String[] Maintainer { get; set; }
+
+        public static void SaveStatic(Settings s)
+        {
+            var jsonSerializerOptions = new JsonSerializerOptions();
+            jsonSerializerOptions.WriteIndented = true;
+            var json = JsonSerializer.Serialize(s, jsonSerializerOptions);
+            File.WriteAllText(SettingsPath, json);
+        }
+
+        public void SaveInstance()
+        {
+            var jsonSerializerOptions = new JsonSerializerOptions();
+            jsonSerializerOptions.WriteIndented = true;
+            var json = JsonSerializer.Serialize(this, jsonSerializerOptions);
+            File.WriteAllText(SettingsPath, json);
+        }
+
+        public static Settings Load()
+        {
+            var json = File.ReadAllText(SettingsPath);
+            var settings = JsonSerializer.Deserialize< Settings>(json);
+            return settings;
+        }
+    }
+}
