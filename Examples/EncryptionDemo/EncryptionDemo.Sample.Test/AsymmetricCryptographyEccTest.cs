d3f20fa8089534071979ec17b4d85b4fce39e089
Daniel <dhcgn@users.noreply.github.com>
2020-02-25 16:27:39 +0100

    Getting started with ECC trvia samples

---
+++ Examples/EncryptionDemo/EncryptionDemo.Sample.Test/AsymmetricCryptographyEccTest.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentAssertions;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace EncryptionDemo.Sample.Test
+{
+    public class AsymmetricCryptographyEccTest
+    {
+        private readonly ITestOutputHelper outputHelper;
+
+        public AsymmetricCryptographyEccTest(ITestOutputHelper outputHelper)
+        {
+            this.outputHelper = outputHelper;
+        }
+
+        [Fact]
+        public void CreateKeyPair()
+        {
+            #region Arange
+
+
+
+            #endregion
+
+            #region Act
+
+            var keyPair = AsymmetricCryptographyEcc.CreateKeyPair();
+
+            outputHelper.WriteLine(Convert.ToBase64String(keyPair.privateKeyInfo));
+            outputHelper.WriteLine(Convert.ToBase64String(keyPair.subjectPublicKeyInfo));
+
+            #endregion
+
+            #region Assert
+
+            keyPair.privateKeyInfo.Should().NotEqual(keyPair.subjectPublicKeyInfo);
+            keyPair.privateKeyInfo.Should().HaveCountGreaterThan(keyPair.subjectPublicKeyInfo.Length);
+
+            #endregion
+        }
+
+        [Fact]
+        public void GetPublicKey()
+        {
+            #region Arange
+
+            var keyPair = AsymmetricCryptographyEcc.CreateKeyPair();
+
+            #endregion
+
+            #region Act
+
+            var publicKey = AsymmetricCryptographyEcc.GetPublicKey(keyPair.privateKeyInfo);
+
+            outputHelper.WriteLine(Convert.ToBase64String(keyPair.subjectPublicKeyInfo));
+            outputHelper.WriteLine(Convert.ToBase64String(publicKey));
+
+            #endregion
+
+            #region Assert
+
+            publicKey.Should().Equal(keyPair.subjectPublicKeyInfo);
+
+            #endregion
+        }
+
+        [Fact]
+        public void Derive()
+        {
+            #region Arange
+
+            var keyPairAlive = AsymmetricCryptographyEcc.CreateKeyPair();
+            var keyPairBob = AsymmetricCryptographyEcc.CreateKeyPair();
+
+            #endregion
+
+            #region Act
+
+            var deriveSecretA = AsymmetricCryptographyEcc.DeriveSecret(keyPairAlive.privateKeyInfo, keyPairBob.subjectPublicKeyInfo);
+            var deriveSecretB = AsymmetricCryptographyEcc.DeriveSecret(keyPairBob.privateKeyInfo, keyPairAlive.subjectPublicKeyInfo);
+
+            #endregion
+
+            #region Assert
+
+            deriveSecretA.Should().Equal(deriveSecretB);
+
+            #endregion
+        }
+
+
+        [Fact]
+        public void SignData()
+        {
+            #region Arange
+
+            var keyPairAlive = AsymmetricCryptographyEcc.CreateKeyPair();
+
+            var dataA = Guid.Empty.ToByteArray();
+            var dataB = Guid.Empty.ToByteArray();
+
+            #endregion
+
+            #region Act
+
+            var signatureA = AsymmetricCryptographyEcc.SignData(keyPairAlive.privateKeyInfo, dataA);
+            var signatureB = AsymmetricCryptographyEcc.SignData(keyPairAlive.privateKeyInfo, dataA);
+
+            outputHelper.WriteLine(Convert.ToBase64String(signatureA));
+            outputHelper.WriteLine(Convert.ToBase64String(signatureB));
+            outputHelper.WriteLine("");
+            outputHelper.WriteLine(BitConverter.ToString(signatureA).Replace("-", "").ToLower());
+            outputHelper.WriteLine(BitConverter.ToString(signatureB).Replace("-", "").ToLower());
+
+            #endregion
+
+            #region Assert
+
+            // Todo why?
+            signatureB.Should().HaveCount(512 / 8 * 2);
+            // Todo why?
+            signatureA.Should().NotEqual(signatureB);
+            
+
+            #endregion
+        }
+    }
+}
---
+++ Examples/EncryptionDemo/EncryptionDemo.Sample/AsymmetricCryptographyEcc.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace EncryptionDemo.Sample
+{
+    public class AsymmetricCryptographyEcc
+    {
+        // See
+        private static readonly ECCurve NamedCurveBrainpoolP512R1 = ECCurve.NamedCurves.brainpoolP512r1;
+        // See
+        private static readonly HashAlgorithmName DeriveHashAlgorithmName = HashAlgorithmName.SHA512;
+        // See
+        private static readonly HashAlgorithmName SignHashAlgorithmName = HashAlgorithmName.SHA512;
+
+        /// <summary>
+        /// Generates and exports the current key in the PKCS#8 PrivateKeyInfo format.
+        /// </summary>
+        /// <returns></returns>
+        public static (byte[] privateKeyInfo, byte[] subjectPublicKeyInfo) CreateKeyPair()
+        {
+            var curve = ECDiffieHellman.Create(NamedCurveBrainpoolP512R1);
+            return (curve.ExportPkcs8PrivateKey(), curve.ExportSubjectPublicKeyInfo());
+        }
+
+        /// <summary>
+        /// Exports the public-key portion of the current key in the X.509 SubjectPublicKeyInfo format.
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public static byte[] GetPublicKey(byte[] key)
+        {
+            var curve = ECDiffieHellman.Create(NamedCurveBrainpoolP512R1);
+            curve.ImportPkcs8PrivateKey(key, out var _);
+
+            var ecDsa = ECDsa.Create(NamedCurveBrainpoolP512R1);
+            ecDsa.ImportPkcs8PrivateKey(key, out var _);
+         
+            return curve.ExportSubjectPublicKeyInfo();
+        }
+
+        public static byte[] DeriveSecret(byte[] privateKeyInfo, byte[] subjectPublicKeyInfo)
+        {
+            var curvePublic = ECDiffieHellman.Create(NamedCurveBrainpoolP512R1);
+            curvePublic.ImportSubjectPublicKeyInfo(subjectPublicKeyInfo, out var _);
+
+            var curvePrivate = ECDiffieHellman.Create(NamedCurveBrainpoolP512R1);
+            curvePrivate.ImportPkcs8PrivateKey(privateKeyInfo, out var _);
+
+            return curvePrivate.DeriveKeyFromHash(curvePublic.PublicKey, DeriveHashAlgorithmName);
+        }
+
+        public static byte[] SignData(byte[] privateKeyInfo, byte[] data)
+        {
+            var ecDsa = ECDsa.Create(NamedCurveBrainpoolP512R1);
+            ecDsa.ImportPkcs8PrivateKey(privateKeyInfo, out var _);
+
+            return ecDsa.SignData(data, SignHashAlgorithmName);
+        }
+
+        public static bool VerifyData(byte[] subjectPublicKeyInfo, byte[] data, byte[] signature)
+        {
+            var ecDsa = ECDsa.Create(NamedCurveBrainpoolP512R1);
+            ecDsa.ImportSubjectPublicKeyInfo(subjectPublicKeyInfo, out var _);
+
+            return ecDsa.VerifyData(data, signature, SignHashAlgorithmName);
+        }
+    }
+}
