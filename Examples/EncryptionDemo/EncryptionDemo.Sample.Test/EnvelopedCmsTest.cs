d1617f291be115b4b95ff69a70b2d13311eb0602
Daniel <dhcgn@users.noreply.github.com>
2020-02-25 22:07:01 +0100

    Add CMS CryptographicMessageSyntax

---
+++ Examples/EncryptionDemo/EncryptionDemo.Sample.Test/EnvelopedCmsTest.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Security;
+using System.Text;
+using System.Security.Cryptography.Pkcs;
+using System.Security.Cryptography.X509Certificates;
+using FluentAssertions;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace EncryptionDemo.Sample.Test
+{
+    public class EnvelopedCmsTest
+    {
+        private readonly ITestOutputHelper outputHelper;
+
+        public EnvelopedCmsTest(ITestOutputHelper outputHelper)
+        {
+            this.outputHelper = outputHelper;
+        }
+
+        private X509Certificate2 ReadX509Certificate2(int i)
+        {
+            var executingAssembly = Assembly.GetExecutingAssembly();
+            var name = executingAssembly.GetManifestResourceNames()
+                .First(s => s.EndsWith($"Certificate_{i}_pw_qwert.pfx"));
+
+            var ms = new MemoryStream();
+            executingAssembly.GetManifestResourceStream(name).CopyTo(ms);
+
+            var secureString = new SecureString();
+            foreach (var item in "qwert")
+            {
+                secureString.AppendChar(item);
+            }
+            return new X509Certificate2(ms.ToArray(), secureString);
+        }
+
+
+        [Fact]
+        public void Encrypt()
+        {
+            var data = Guid.NewGuid().ToByteArray();
+            var cert = ReadX509Certificate2(1);
+
+            var cms = CryptographicMessageSyntax.Encrypt(data, cert);
+
+            data.Should().NotEqual(cms);
+        }
+
+        [Fact]
+        public void Decrypt()
+        {
+            var data = Guid.NewGuid().ToByteArray();
+            var cert = ReadX509Certificate2(1);
+
+            var cms = CryptographicMessageSyntax.Encrypt(data, cert);
+            var plain = CryptographicMessageSyntax.Decrypt(cms, cert);
+
+            outputHelper.WriteLine(cms.Length+": "+Convert.ToBase64String(cms));
+
+            data.Should().NotEqual(cms);
+            data.Should().Equal(plain);
+        }
+    }
+}
---
+++ Examples/EncryptionDemo/EncryptionDemo.Sample/CryptographicMessageSyntax.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Security.Cryptography.Pkcs;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace EncryptionDemo.Sample
+{
+    public class CryptographicMessageSyntax
+    {
+        public static byte[] Encrypt(byte[] data, params X509Certificate2[] certs)
+        {
+            var envelopedCms = new EnvelopedCms(new ContentInfo(data));
+            if (envelopedCms.ContentEncryptionAlgorithm.Oid.FriendlyName != "aes256")
+            {
+                throw new Exception("Only aes256 is allowed.");
+            }
+
+            var cmsRecipientCollection = new CmsRecipientCollection();
+            foreach (var x509Certificate2 in certs)
+            {
+                cmsRecipientCollection.Add(new CmsRecipient(x509Certificate2, RSAEncryptionPadding.OaepSHA512));
+            }
+            envelopedCms.Encrypt(cmsRecipientCollection);
+
+            return envelopedCms.Encode();
+        }
+
+        public static byte[] Decrypt(byte[] data, params X509Certificate2[] certs)
+        {
+            var envelopedCms = new EnvelopedCms();
+            envelopedCms.Decode(data);
+
+            envelopedCms.Decrypt(new X509Certificate2Collection(certs));
+
+            return envelopedCms.Encode();
+        }
+    }
+}
