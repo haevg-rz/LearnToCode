ce3ee85e2d70aa52b20ec398d26c32819d6235bc
Daniel <dhcgn@users.noreply.github.com>
2020-02-26 09:04:43 +0100

    Add JavascriptObjectSigningAndEncryption

---
+++ Examples/EncryptionDemo/EncryptionDemo.Sample.Test/JavascriptObjectSigningAndEncryptionTest.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentAssertions;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace EncryptionDemo.Sample.Test
+{
+    public class JavascriptObjectSigningAndEncryptionTest
+    {
+        private readonly ITestOutputHelper outputHelper;
+
+        public JavascriptObjectSigningAndEncryptionTest(ITestOutputHelper outputHelper)
+        {
+            this.outputHelper = outputHelper;
+        }
+
+        [Fact]
+        public void Encrypt()
+        {
+            #region Arange
+
+            var key = Sample.KeyGeneration.CreateRandom(256);
+            var plain = Guid.NewGuid().ToString();
+
+            #endregion
+
+            #region Act
+
+            var cipherText = JavascriptObjectSigningAndEncryption.Encrypt(key, plain);
+
+            #endregion
+
+            #region Assert
+
+            outputHelper.WriteLine("Token: "+ cipherText);
+            outputHelper.WriteLine("Secret: "+ Convert.ToBase64String(key)+ " (Base64Encode)");
+            outputHelper.WriteLine("Secret: " + Microsoft.AspNetCore.WebUtilities.WebEncoders.Base64UrlEncode(key)+ " (Base64UrlEncode)");
+
+            plain.Should().NotBe(cipherText);
+
+            #endregion
+        }
+
+        [Fact]
+        public void Decrypt()
+        {
+            #region Arange
+
+            var key = Sample.KeyGeneration.CreateRandom(256);
+            var plain = Guid.NewGuid().ToString();
+            var token = JavascriptObjectSigningAndEncryption.Encrypt(key, plain);
+
+            #endregion
+
+            #region Act
+
+            var decrypted = JavascriptObjectSigningAndEncryption.Decrypt(key, token);
+
+            #endregion
+
+            #region Assert
+
+            outputHelper.WriteLine("Token: "+ token);
+            outputHelper.WriteLine("Secret: "+ Convert.ToBase64String(key)+ " (Base64Encode)");
+            outputHelper.WriteLine("Secret: " + Microsoft.AspNetCore.WebUtilities.WebEncoders.Base64UrlEncode(key)+ " (Base64UrlEncode)");
+
+            plain.Should().NotBe(token);
+            decrypted.Should().Be(plain);
+
+            #endregion
+        }
+
+    }
+}
---
+++ Examples/EncryptionDemo/EncryptionDemo.Sample/JavascriptObjectSigningAndEncryption.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Jose;
+
+namespace EncryptionDemo.Sample
+{
+    public class JavascriptObjectSigningAndEncryption
+    {
+        public static string Encrypt(byte[] secret, string plainText)
+        {
+            // Claims see https://tools.ietf.org/html/rfc7519#section-4.1
+            var nonEncryptedHeaders = new Dictionary<string, object>()
+            {
+                { "sub", "mr.x@contoso.com" },
+                { "exp", 1300819380 }
+            };
+
+            var token = Jose.JWT.Encode(plainText, secret, JweAlgorithm.A256KW, JweEncryption.A256GCM,extraHeaders: nonEncryptedHeaders);
+            return token;
+        }
+
+        public static string Decrypt(byte[] secret, string token)
+        {
+            var r = Jose.JWT.Decode(token, secret, JweAlgorithm.A256KW, JweEncryption.A256GCM);
+            return r;
+        }
+    }
+}
