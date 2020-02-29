e761711e088a441e60fe75d223daef677f80d73c
Daniel <dhcgn@secure.mailbox.org>
2020-02-29 14:41:19 +0100

    Update AsymmetricCryptographyRsa.cs
    Add Documentation

--- Examples/EncryptionDemo/EncryptionDemo.Sample/AsymmetricCryptographyRsa.cs
+++ Examples/EncryptionDemo/EncryptionDemo.Sample/AsymmetricCryptographyRsa.cs
@@ -8,21 +8,37 @@ namespace EncryptionDemo.Sample
     public class AsymmetricCryptographyRsa
     {
         // see BSI TR-02102-1 
+#if RELEASE
         private const int RsaKeySize = 1024 * 4;
+#else
+        private const int RsaKeySize = 1024 * 2;
+#endif
 
-        // see BSI TR-02102-1 
+        /// <summary>
+        /// Gets an object that represents the Optimal Asymmetric Encryption Padding (OAEP) encryption standard with a SHA512 hash algorithm.
+        /// </summary>
+        /// <remarks>
+        /// see BSI TR-03116-1, 3.2 RSA, Allowed until 2024+
+        /// </remarks>
         private static readonly RSAEncryptionPadding RsaEncryptionPadding = RSAEncryptionPadding.OaepSHA512;
 
-        // see BSI TR-02102-1 
+
+        /// <summary>
+        ///  The padding mode for RSA signature Pss - Probabilistic Signature Scheme (RSASSA-PSS) of the PKCS #1: RSA Encryption Standard.
+        ///  </summary>
+        ///  <remarks>
+        ///  see BSI TR-03116-1, 3.2 RSA, Allowed until 2024+ and
+        ///  recommended after BSI TR-02102-1, 1.4. Handling of legacy algorithm, 4. RSA with PKCS1v1.5 padding and 
+        ///  BSI TR-02102-1, 5.4.1. RSA, Table 5.4.: Recommended padding schemes for the RSA signature algorithm
+        ///  </remarks>
         private static readonly RSASignaturePadding RsaSignaturePadding = RSASignaturePadding.Pss;
 
         // see BSI TR-02102-1 
-        private static readonly HashAlgorithmName SignaturehashAlgorithmName = HashAlgorithmName.SHA512;
+        private static readonly HashAlgorithmName SignatureHashAlgorithmName = HashAlgorithmName.SHA512;
 
         public static string CreateRsaKeyPair()
         {
-            using var rsa = RSA.Create();
-            rsa.KeySize = RsaKeySize;
+            using var rsa = RSA.Create(RsaKeySize);
 
             return rsa.ToXmlString(true);
         }
@@ -58,7 +74,7 @@ namespace EncryptionDemo.Sample
             using var rsa = RSA.Create();
             rsa.FromXmlString(rsaXml);
             // Todo check prvivate key
-            var signature = rsa.SignData(data, SignaturehashAlgorithmName, RsaSignaturePadding);
+            var signature = rsa.SignData(data, SignatureHashAlgorithmName, RsaSignaturePadding);
             return signature;
         }
 
@@ -67,7 +83,7 @@ namespace EncryptionDemo.Sample
             using var rsa = RSA.Create();
             rsa.FromXmlString(rsaXml);
             // Todo check private key
-            var isVerifedData = rsa.VerifyData(data, signature, SignaturehashAlgorithmName, RsaSignaturePadding);
+            var isVerifedData = rsa.VerifyData(data, signature, SignatureHashAlgorithmName, RsaSignaturePadding);
             return isVerifedData;
         }
     }
