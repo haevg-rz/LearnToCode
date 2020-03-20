cffd679352d97c444c52269eb60f4a93a451bf89
Daniel <dhcgn@users.noreply.github.com>
2020-03-20 11:15:51 +0100

    Create ViewModel.cs

---
+++ Examples/MVVM Binding/ViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MVVM_Binding
+{
+    public class ViewModel
+    {
+        public string Content1 { get; set; } = "Hallo";
+    }
+}
