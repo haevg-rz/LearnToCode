514bcec2671f2a5dbd353b1654aef443ac4a2261
Daniel <dhcgn@users.noreply.github.com>
2020-03-20 11:43:21 +0100

    Add Eventhandling

--- Examples/MVVM Binding/ViewModel.cs
+++ Examples/MVVM Binding/ViewModel.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace MVVM_Binding
 {
-    public class ViewModel
+    public class ViewModel : INotifyPropertyChanged
     {
         public string Content1 { get; set; } = "Hallo";
         public string Content2 { get; set; }
@@ -12,6 +14,13 @@ namespace MVVM_Binding
         public string Content2TextBlock
         {
             get => "#: "+ Content2;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
