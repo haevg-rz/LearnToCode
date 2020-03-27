b1abace6ef3bea7b63ad2af0e5329bff96ebd87b
kirill.serebryakov <kirill.serebryakov@haevg-rz.de>
2020-03-27 11:36:51 +0100

    Gherkin code example added

---
+++ Examples/GherkinExample/TaschenrechnerCore/TaschenrechnerLogik.cs
@@ -0,0 +1,14 @@
+﻿namespace TaschenrechnerCore
+{
+    public class TaschenrechnerLogik
+    {
+        public static int Zahl1 { get; set; }
+        public static int Zahl2 { get; set; }
+        public static int Result { get; set; }
+
+        public static void AddTwoNumbers()
+        {
+            Result = Zahl1 + Zahl2;
+        }
+    }
+}
---
+++ Examples/GherkinExample/TaschenrechnerGui/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using TaschenrechnerCore;
+
+namespace TaschenrechnerGui
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Geben sie die erste Zahl ein: ");
+            TaschenrechnerLogik.Zahl1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Geben sie die zweite Zahl ein: ");
+            TaschenrechnerLogik.Zahl2 = Convert.ToInt32(Console.ReadLine());
+
+            //Simulates pressing the add button
+            TaschenrechnerLogik.AddTwoNumbers();
+
+            Console.WriteLine(
+                $"Das Ergebnis der Addition der beiden Zahlen ist: {TaschenrechnerLogik.Result}");
+
+            Console.ReadLine();
+        }
+    }
+}
